8334719860d3dd0395c5b92781884e1030a68a60
DannyCoop <cooperdaniel5141@gmail.com>
2020-08-14 14:21:21 -0400

    goes witht he last commit

---
+++ src/components/NavButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import '../NavButton.css'
+
+export default function NavButton() {
+    return (
+        <div className="Nav-button">
+            This works
+        </div>
+    )
+}
+
+
