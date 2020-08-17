988eafe3e98f315d5ce4fe7803868eaed7623fb3
DannyCoop <cooperdaniel5141@gmail.com>
2020-08-17 17:08:49 -0400

    merging some of my changes

--- src/components/Event/EventDetail.jsx
+++ src/components/Event/EventDetail.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./EventDetail.css";
 import "../EventUi.css"
 
 export const EventDetail = (props) => {
--- src/containers/EventCollection.js
+++ src/containers/EventCollection.js
@@ -1,40 +1,18 @@
 import React, { Component } from 'react'
 import EventDetail from '../components/Event/EventDetail';
 import EventCard from '../components/EventCard/EventCard.jsx';
+import EventModal from '../components/EventModal/EventModal'
 
 export default class EventCollection extends Component {
-    // showEvents = () => {
-    //     return this.props.displayEvents.map(event => {
-    //         return <EventDetail 
-    //         key = {event.id}
-    //         event = {event}
-    //         filterEvents = {this.props.filterEvents}
-    //         />
-    //     })
-    // }
+
 
     render() {
         return (
-            <div>
+            <div className="trigger">
                 {/* {this.showEvents()} */}
                 {this.props.displayEvents.map(event =>  <EventCard key = {event.id} event = {event} filterEvents={this.props.filterEvents}/>)}
+                {this.props.displayEvents.map(event =>  <EventModal key = {event.id} event = {event} filterEvents={this.props.filterEvents}/>)}
             </div>
         )
     }
-<<<<<<< HEAD
 }
-
-// // let showEvents = () => {
-//     return props.displayEvents.map(event => {
-//         return <EventDetail 
-//         key = {event.id}
-//         event = {event}
-//         filterEvents = {props.filterEvents}
-//         />
-//     })
-// }
-
-// 
-=======
-}
->>>>>>> 4dcc0e4fcf0ec3eef593f224b97b7e06c294018b
