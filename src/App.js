6ad73c32ceecbc4c5445ab3b733cfc626361795a
Tien Phan <tienphan1989@gmail.com>
2020-08-17 13:36:21 -0400

    Added images and panels

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <br></br>
-        <BodyExample/>
+        <Body/>
         <Footer/>
       </div>
     );
