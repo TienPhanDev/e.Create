c33481cddbecf7f9eaf6e5093e7ceaeb9d4af0f5
Diwo777 <diego.zegarra79@icloud.com>
2020-08-19 22:35:44 -0400

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,15 @@ import NavButton from './NavButton';
 import './Header.css';
 import logo from './logofinal.png';
 import { NavLink } from 'react-router-dom'
+import Logo from './lastlogo.png'
+import ecreate from './ecreate.png'
 
 export default function Header() {
     
     return (
         <div className="Header-container">
-            <img src={logo} alt=''></img>
+            <img className="logo" src={Logo} alt=''></img>
+            <img className="ecreate" src={ecreate} alt=''></img>
             <div>
                <NavLink to="/login" > <NavButton name={"Login"} />   </NavLink> 
                <NavLink to="/event_form"><NavButton name={"Create Event"}/></NavLink>
