9661dd33f231a992ffe2111692a4900412e0c95a
Tien Phan <tienphan1989@gmail.com>
2020-08-19 21:31:21 -0400

    "Fixed scrolling issue"

--- src/components/EventModal/EventModal.js
+++ src/components/EventModal/EventModal.js
@@ -30,23 +30,6 @@ const EventModal = (props) =>{
                 </button>
             </div>
         </div>
-
-
-
-
-
-        // <div className="event-card" onClick={(event) => props.filterEvents(props.event)}>
-        //     <img src={"https://www.alpha.network/sites/default/files/2019-07/alpha-spring-cocktail-party-event-thumbnail-new_0.jpg"} alt=""></img>
-        //     <h2>Event title: {title}</h2>
-        //     <div className="card-info">
-        //     <h4>Event price: ${price}</h4>
-        //     <h4>Tickets left for sale:" "</h4>
-        //     <h4>Description: " "</h4>
-        //     <div>Date: {date}</div>
-        //     <div>Location: {location}</div>
-        //     <button onClick={() => console.log("You clicked the buy a ticket button, this event ID is:", {id})}>CLICK HERE TO GET A TICKET!</button>
-        //     </div>
-        // </div>
     )
 }
 
