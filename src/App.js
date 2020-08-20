b7bf460f6da70ceb4afd77d3651ce2619944eeef
Diwo777 <diego.zegarra79@icloud.com>
2020-08-19 21:57:06 -0400

    buttons

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ import { Route } from 'react-router-dom';
 import UserForm from './components/UserForm/UserForm';
 import EventForm from './components/EventForm/EventForm';
 import UserHome from "./components/UserHomepage/UserLanding";
+import EventCollection from './containers/EventCollection';
+import AllCategories from './containers/AllCategories';
+
 
 const App = () => { 
   
     return (
       <div className="App">
         <Header/>
+        {/* <Route path="/" component={Body} /> */}
+        <Route exact path='/' component={Body}/>
         <Route path="/events" component={Body}/>
         <Route path="/home" component={UserHome}/>
         <Route path="/login" component={UserForm}/>
